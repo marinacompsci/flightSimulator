f4bafdf9ad2aad3b766c3ece6a0ad64d11639e79
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-12-26 14:34:22 +0100

    added control buttons and moving functionality

--- flugSimulator/View Controllers/GameVC.swift
+++ flugSimulator/View Controllers/GameVC.swift
@@ -10,14 +10,17 @@ import UIKit
 class GameVC: UIViewController {
     
     let airplane = Airplane(image: UIImageView(image: UIImage(named: "airplane")))
-    var cloud = Cloud(image: UIImageView(image: UIImage(named: "cloud")))
-    let gameOver = false
+    let cloud = Cloud(image: UIImageView(image: UIImage(named: "cloud")))
+    let rightArrow = UIButton()
+    let leftArrow = UIButton()
     var backgroundView: UIImageView!
     
     let timeLabel = UILabel()
-
     let distanceLabel = UILabel()
     var distanceTravelled = 0.0
+    
+    let gameOver = false
+    var airplaneXShift = CGFloat(0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,7 @@ class GameVC: UIViewController {
         setupBackground()
         setupLabels()
         setupClouds()
+        setupControlButtons()
         setupAirplane()
         gameEngine()
     }
@@ -65,8 +69,6 @@ class GameVC: UIViewController {
             timeLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
             timeLabel.topAnchor.constraint(equalTo: distanceLabel.bottomAnchor, constant: 10),
         ])
-        
-        
     }
     
     private func setupAirplane() {
@@ -75,7 +77,7 @@ class GameVC: UIViewController {
         
         NSLayoutConstraint.activate([
             airplane.image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            airplane.image.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
+            airplane.image.bottomAnchor.constraint(equalTo: rightArrow.topAnchor, constant: -5),
             airplane.image.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.3),
             airplane.image.heightAnchor.constraint(equalTo: airplane.image.widthAnchor),
         ])
@@ -90,6 +92,34 @@ class GameVC: UIViewController {
             cloud.image.topAnchor.constraint(equalTo: distanceLabel.topAnchor),
             cloud.image.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.3),
             cloud.image.heightAnchor.constraint(equalTo: cloud.image.widthAnchor),
+        ])
+    }
+    
+    private func setupControlButtons() {
+        view.addSubview(rightArrow)
+        view.addSubview(leftArrow)
+        rightArrow.tintColor = .black
+        rightArrow.setBackgroundImage(UIImage(systemName: "chevron.right.circle.fill"), for: .normal)
+        rightArrow.addTarget(self, action: #selector(moveAirplaneToRight), for: .touchUpInside)
+        
+        leftArrow.tintColor = .black
+        leftArrow.setBackgroundImage(UIImage(systemName: "chevron.left.circle.fill"), for: .normal)
+        leftArrow.addTarget(self, action: #selector(moveAirplaneToLeft), for: .touchUpInside)
+        
+        rightArrow.translatesAutoresizingMaskIntoConstraints = false
+        leftArrow.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            leftArrow.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            leftArrow.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
+            leftArrow.heightAnchor.constraint(equalToConstant: view.bounds.width * 0.20),
+            leftArrow.widthAnchor.constraint(equalTo: leftArrow.heightAnchor),
+            
+            rightArrow.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            rightArrow.bottomAnchor.constraint(equalTo: leftArrow.bottomAnchor),
+            rightArrow.heightAnchor.constraint(equalTo: leftArrow.heightAnchor),
+            rightArrow.widthAnchor.constraint(equalTo: leftArrow.widthAnchor),
         ])
     }
     
@@ -112,6 +142,7 @@ class GameVC: UIViewController {
         timeLabel.text = "Time: \(Int(timer))s"
         distanceTravelled += 0.03 * airplane.speed
         distanceLabel.text = "Distance behind: \(String(format: "%.2f", distanceTravelled))km"
+        
         UIView.animate(withDuration: 1, animations: {
             [weak self] in
             self?.cloud.image.transform = CGAffineTransform(translationX: 0, y: CGFloat(yPosition*100))
@@ -126,5 +157,24 @@ class GameVC: UIViewController {
     }
 
 
+    @objc
+    private func moveAirplaneToRight() {
+        UIView.animate(withDuration: 0.1, animations: {
+            [weak self] in
+            self?.airplaneXShift += 8.0
+            self?.airplane.image.transform = CGAffineTransform(translationX: self!.airplaneXShift, y: 0)
+        })
+    }
+    
+    @objc
+    private func moveAirplaneToLeft() {
+        UIView.animate(withDuration: 0.1, animations: {
+            [weak self] in
+            self?.airplaneXShift -= 8.0
+            self?.airplane.image.transform = CGAffineTransform(translationX: self!.airplaneXShift, y: 0)
+        })
+    }
+    
+    //TODO: HOLD BUTTON -> MOVE AIRPLANE FASTER
 }
 
